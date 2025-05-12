73037a04f24c2eadd4d8db5a7b8fbdb9eb3f7b35
Eui-chan Jung <heueum22@gmail.com>
2025-05-12 15:52:31 +0900

    feat: alb listner 추가

--- terraform/modules/ec2/alb/alb/alb.tf
+++ terraform/modules/ec2/alb/alb/alb.tf
@@ -11,3 +11,35 @@ resource "aws_lb" "alb" {
     env = var.env
   }
 }
+
+resource "aws_lb_listener" "http_listner" {
+  load_balancer_arn = aws_lb.alb.arn
+  port = "80"
+  protocol = "HTTP"
+  
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https_listner" {
+  load_balancer_arn = aws_lb.alb.arn
+  port = "443"
+  protocol = "HTTPS"
+  
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
